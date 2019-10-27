be48ad9b37396557b4cafda372e3585161d59ffc
varunkat <varun.sky56@gmail.com>
2019-10-26 22:55:22 -0400

    updated the code

--- app/src/main/java/com/example/p4/MainActivity.java
+++ app/src/main/java/com/example/p4/MainActivity.java
@@ -1,14 +1,64 @@
-package com.example.p4;
+package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+    private Button button6;
+    private Button length;
+    private Button mass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
+
+
+        button6 = (Button) findViewById(R.id.button6);
+        length = (Button) findViewById(R.id.length);
+        mass = (Button) findViewById(R.id.mass);
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                opentempconvert();
+            }
+        });
+        length.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openlenconv();
+            }
+        });
+        mass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openmassconv();
+            }
+        });
     }
+
+    public void opentempconvert() {
+        Intent intent = new Intent(this,tempconvert.class);
+        startActivity(intent);
+    }
+
+    public void openlenconv() {
+        Intent intent = new Intent(this,lenconv.class);
+        startActivity(intent);
+    }
+    public void openmassconv() {
+        Intent intent = new Intent(this,massconv.class);
+        startActivity(intent);
+    }
+
 }
